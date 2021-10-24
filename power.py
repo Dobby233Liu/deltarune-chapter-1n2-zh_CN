557429abe033c760c6176f0f998f32db1fa8baca
Liu Wenyuan <15816141883@163.com>
2021-10-24 12:46:43 +0800

    Update power.py

--- power.py
+++ power.py
@@ -9,6 +9,8 @@ out = open("hyperlink_blocked.txt", "w")
 for txtc, txt in j.items():
     txtl = txt.lower()
     if "hyperlink" in txtl or "blocked" in txtl:
-        out.write("%s\n%s\n\n" % txtc, txt)
+        o = ("%s\n%s\n\n" % (txtc, txt))
+        print(o)
+        out.write(o)
 
 out.close()
